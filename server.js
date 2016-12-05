a5c27f00b8b36744c8f6467b288f0b1045b89184
Coy Sanders <coymeetsworld@gmail.com>
2016-12-04 22:37:25 -0800

    Update middleware for localhost

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const PORT = process.env.PORT || 3000; // way to access environment variables th
 */
 app.use(function (req, res, next) {
 		
-	if (req.headers['x-forwarded-proto'] === 'http')	{
-		next();	
-	} else {
+	if (req.headers['x-forwarded-proto'] === 'https')	{
 		res.redirect('http://' + req.hostname + req.url);	
+	} else {
+		next();	
 	}
 });
 
