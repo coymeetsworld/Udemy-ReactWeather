cba8628c94fa5fe9ee137f8a48eaf8df82519da7
Coy Sanders <coymeetsworld@gmail.com>
2016-12-04 22:45:00 -0800

    Rename about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 /* Stateless functional component. This can be done if the component manages no state and only just renders (i.e. strictly presentational). */
 var About = (props) => {
 	return (
-		<h3>About Component</h3>
+		<div>
+			<h3>About</h3>
+			<p>Welcome to the About page!</p>
+		</div>
 	);
 }
 
