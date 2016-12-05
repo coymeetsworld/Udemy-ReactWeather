768e752db578bf1a7da277d8b770e237e4fa5cf1
Coy Sanders <coymeetsworld@gmail.com>
2016-12-04 22:25:28 -0800

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,9 +3,28 @@ var express = require('express');
 // Create our app
 var app = express();
 
+const PORT = process.env.PORT || 3000; // way to access environment variables through node.
+
+/*
+	Redirects https to http
+	req: could be index.html, or bundle.js, etc
+	res: update what gets sent back
+	next: can be called when this piece of middleware is done
+*/
+app.use(function (req, res, next) {
+		
+	if (req.headers['x-forwarded-proto'] === 'http')	{
+		next();	
+	} else {
+		res.redirect('http://' + req.hostname + req.url);	
+	}
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, function () { 
-	console.log("Express server is up on port 3000");
+//Heroku will tell us what port we need to bind to via a environment variable
+//app.listen(3000, function () { 
+app.listen(port, function () { 
+	console.log('Express server is up on port ' + port);
 });
 
